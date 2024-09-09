a6db6958feae052f18738ce9ff200bfa4b682647
Sebastian Paniagua <spaniagua@hugeinc.com>
2024-09-09 11:01:39 -0500

    Installing postgresql database adaptor

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and returns the result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and returns the result."""
+    return y - x
